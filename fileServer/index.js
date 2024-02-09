c7968b9ece4e425ac82fef58a7cfa8d52aecadf4
piyush016 <piyushcool116@gmail.com>
2024-02-09 11:37:07 +0530

    Add: File server

---
+++ fileServer/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+app.get("/file", (req, res) => {
+  fs.readdir(path.join(__dirname, "./file/"), (err, files) => {
+    if (err) {
+      return res.status(500).json({ error: err });
+    }
+    res.json(files);
+  });
+});
+
+app.get("/file/:filename", (req, res) => {
+  const filepath = path.join(__dirname, "./file/", req.params.filename);
+
+  fs.readFile(filepath, "utf-8", (err, data) => {
+    if (err) {
+      return res.status(404).send("File not found");
+    }
+    res.send(data);
+  });
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Route not found");
+});
+
+app.listen(3000, () => {
+  console.log("Server up and running!");
+});
